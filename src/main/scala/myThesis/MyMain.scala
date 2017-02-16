920fa7ac6bf71df400a1d2644f49924bab4f4f5b
Etrunon <bosottiluca@gmail.com>
2017-02-16 18:43:06 +0100

    CHANGE in spark version. Now use 2.1 Installed Scala 2.10 on machine removed tests and refactored code to work with updated spark

--- src/main/scala/myThesis/MyMain.scala
+++ src/main/scala/myThesis/MyMain.scala
@@ -3,8 +3,9 @@ package myThesis
 import java.io.File
 import java.util.Date
 
-import org.apache.spark.graphx._
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.util.GraphGenerators
 
 /**
   * Created by etrunon on 13/02/17.
@@ -17,7 +18,7 @@ object MyMain {
     // Sets source folder
     val edgeFile = "RunData/Input/mini1.csv"
     // Sets output folder
-    val outputPath = "RunData/Output/" + new java.text.SimpleDateFormat("dd-MM-yyyy_HH:mm").format(new Date())
+    val outputPath = "RunData/Output/" + new java.text.SimpleDateFormat("dd-MM-yyyy_HH:mm:ss").format(new Date())
     val outputDir = new File(outputPath)
     outputDir.mkdirs()
     val edgedelimiter = ","
@@ -43,8 +44,33 @@ object MyMain {
     val tmpGraph: Graph[(Long, Long), Long] = Graph.fromEdges(edgeRDD, (-1L, -1L))
 
     val degrees = tmpGraph.degrees
-    val graph: Graph[(Long, Long, List[Long]), Long] = tmpGraph.outerJoinVertices(degrees) { (id, _, degOpt) => (degOpt.getOrElse(0).toLong, id, List[Long](id)) }
+    //    val graph: Graph[(Long, Long, List[Long]), Long] = tmpGraph.outerJoinVertices(degrees) { (id, _, degOpt) => (degOpt.getOrElse(0).toLong, id, List[Long](id)) }
+
 
     tmpGraph.triplets.collect().foreach(println)
+    /*
+        val graph: Graph[Double, Int] = GraphGenerators.logNormalGraph(sc, numVertices = 100).mapVertices( (id, _) => id.toDouble )
+        // Compute the number of older followers and their total age
+        val olderFollowers: VertexRDD[(Int, Double)] = graph.aggregateMessages[(Int, Double)](
+          triplet => { // Map Function
+            if (triplet.srcAttr > triplet.dstAttr) {
+              // Send message to destination vertex containing counter and age
+              triplet.sendToDst(1, triplet.srcAttr)
+            }
+          },
+          // Add counter and age
+          (a, b) => (a._1 + b._1, a._2 + b._2) // Reduce Function
+        )
+
+        // Divide total age by number of older followers to get average age of older followers
+        val avgAgeOfOlderFollowers: VertexRDD[Double] =
+          olderFollowers.mapValues( (id, value) =>
+            value match { case (count, totalAge) => totalAge / count } )
+        // Display the results
+        avgAgeOfOlderFollowers.collect.foreach(println(_))
+        */
+
+    //Compute the number of neighbours
+    val nneb: RDD[VertexId, Int] = tmpGraph.aggregateMessages[Int](nei => 1, (a, b) => a + b)
   }
 }
